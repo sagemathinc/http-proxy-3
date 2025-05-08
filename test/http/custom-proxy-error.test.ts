d401e585035b8386a2d006710d0b2f0587445943
William Stein <wstein@sagemath.com>
2025-05-08 20:59:31 +0000

    unit test of custom proxy error handler

---
+++ test/http/custom-proxy-error.test.ts
@@ -0,0 +1,48 @@
+/*
+  custom-proxy-error.test.ts: Example of using the custom `proxyError` event.
+  
+pnpm test ./custom-proxy-error.test.ts
+*/
+
+import * as http from "http";
+import * as httpProxy from "../..";
+import getPort from "../get-port";
+
+const CUSTOM_ERROR =
+  "Something went wrong. And we are reporting a custom error message.";
+
+describe("Test proxying over HTTP with latency", () => {
+  let ports;
+  it("gets ports", async () => {
+    ports = { bad: await getPort(), proxy: await getPort() };
+  });
+
+  let servers: any = {};
+  const LATENCY = 250;
+  it("creates servers with bad target", () => {
+    const proxy = httpProxy
+      .createServer({
+        target: `http://localhost:${ports.bad}`,
+        timeout: 100,
+      })
+      .listen(ports.proxy);
+
+    proxy.on("error", (err, req, res) => {
+      res.writeHead(500, {
+        "Content-Type": "text/plain",
+      });
+      res.end(CUSTOM_ERROR);
+    });
+
+    servers.proxy = proxy;
+  });
+
+  it("makes a request and observes the custom error we installed", async () => {
+    const a = await (await fetch(`http://localhost:${ports.proxy}`)).text();
+    expect(a).toEqual(CUSTOM_ERROR);
+  });
+
+  it("Clean up", () => {
+    Object.values(servers).map((x: any) => x?.close());
+  });
+});
